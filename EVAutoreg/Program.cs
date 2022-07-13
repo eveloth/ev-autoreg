b5940cf99a7d71a02d0eccf61e26d630c6ac9dbb
Eveloth <vadim.stepanov.web@gmail.com>
2022-07-13 21:17:38 +0300

    Add a simple event loop to run the app as a service

--- EVAutoreg/Program.cs
+++ EVAutoreg/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Exchange.WebServices.Data;
+using System.Text.RegularExpressions;
+
+namespace EVAutoreg;
+
+class Program
+{
+
+    private volatile static bool _stop = false;
+    public static void Main(string[] args)
+    {
+        Console.CancelKeyPress += new ConsoleCancelEventHandler(OnCtrlCPressed);
+
+        while (!_stop)
+        {
+            //Logic
+        }
+
+    }
+
+    private static void OnCtrlCPressed(object? sender, ConsoleCancelEventArgs e)
+    {
+        _stop = true;
+        Console.WriteLine("SIGINT recieved, shutting down the apllication.");
+    }
+}
