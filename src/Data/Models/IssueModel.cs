c8f7c631b13b79d6183e45a101fea6b6d29f0094
eveloth <vadim.stepanov.web@gmail.com>
2022-09-16 14:25:36 +0300

    refactor: proper null handling for issue models

--- src/Data/Models/IssueModel.cs
+++ src/Data/Models/IssueModel.cs
@@ -1,15 +1,30 @@
-﻿namespace Data.Models;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Data.Models;
 
 public class IssueModel
 {
     public int Id { get; set; }
-    public string IssueNo { get; set; }
+
+    [NotNull]
+    public string? IssueNo { get; set; }
+
     public DateTime DateCreated { get; set; }
+
     public string Author { get; set; } = string.Empty;
+
     public string Company { get; set; } = string.Empty;
-    public string Status { get; set; }
-    public string Priority { get; set; }
+
+    [NotNull]
+    public string? Status { get; set; }
+
+    [NotNull]
+    public string? Priority { get; set; }
+
     public string AssignedGroup { get; set; } = string.Empty;
+
     public string Assignee { get; set; } = string.Empty;
-    public string Description { get; set; }
+
+    [NotNull]
+    public string? Description { get; set; }
 }
--- src/Data/Models/XmlIssueModel.cs
+++ src/Data/Models/XmlIssueModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Serialization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Xml.Serialization;
 
 namespace Data.Models;
 
@@ -6,13 +7,13 @@ namespace Data.Models;
 public class XmlIssueModel
 {
     public int Id { get; set; }
-    [XmlElement("ID")] public string IssueNo { get; set; }
-    [XmlElement("DATE_CREATED")] public string DateCreated { get; set; }
+    [XmlElement("ID"), NotNull] public string? IssueNo { get; set; }
+    [XmlElement("DATE_CREATED"), NotNull] public string? DateCreated { get; set; }
     [XmlElement("ORIGINATOR")] public string Author { get; set; } = string.Empty;
     [XmlElement("CTI_CUST_LIST")] public string Company { get; set; } = string.Empty;
-    [XmlElement("STATUS")] public string Status { get; set; }
-    [XmlElement("PRIORITY")] public string Priority { get; set; }
+    [XmlElement("STATUS"), NotNull] public string? Status { get; set; }
+    [XmlElement("PRIORITY"), NotNull] public string? Priority { get; set; }
     [XmlElement("CTI_ASSIGNEDGROUP")] public string AssignedGroup { get; set; } = string.Empty;
     [XmlElement("ASSIGNED_TO")] public string Assignee { get; set; } = string.Empty;
-    [XmlElement("SHORT_DESCR")] public string Description { get; set; }
+    [XmlElement("SHORT_DESCR"), NotNull] public string? Description { get; set; }
 }
