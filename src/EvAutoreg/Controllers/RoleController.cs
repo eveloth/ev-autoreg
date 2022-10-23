8484f982e0b0f29f0950c601121802d82bb43171
Eveloth <vadim.stepanov.web@gmail.com>
2022-10-23 22:40:19 +0300

    refactor: user file-dcoped namespaces

--- src/EvAutoreg/Controllers/RoleController.cs
+++ src/EvAutoreg/Controllers/RoleController.cs
@@ -7,76 +7,75 @@ using EvAutoreg.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace EvAutoreg.Controllers
+namespace EvAutoreg.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoleController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class RoleController : ControllerBase
+    private readonly IUserRolesRepository _userRolesRepository;
+    private readonly IUserRepository _userRepository;
+
+    public RoleController(IUserRolesRepository userRolesRepository, IUserRepository userRepository)
     {
-        private readonly IUserRolesRepository _userRolesRepository;
-        private readonly IUserRepository _userRepository;
+        _userRolesRepository = userRolesRepository;
+        _userRepository = userRepository;
+    }
 
-        public RoleController(IUserRolesRepository userRolesRepository, IUserRepository userRepository)
-        {
-            _userRolesRepository = userRolesRepository;
-            _userRepository = userRepository;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetRoles()
+    {
+        var roles = await _userRolesRepository.GetRoles();
 
-        [HttpGet]
-        public async Task<IActionResult> GetRoles()
-        {
-            var roles = await _userRolesRepository.GetRoles();
+        return Ok(roles);
+    }
 
-            return Ok(roles);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> AddRole(RoleDto role)
-        {
-            var roleName = role.RoleName.ToLower();
+    [HttpPost]
+    public async Task<IActionResult> AddRole(RoleDto role)
+    {
+        var roleName = role.RoleName.ToLower();
             
-            await _userRolesRepository.AddRole(roleName);
+        await _userRolesRepository.AddRole(roleName);
 
-            return Ok($"{roleName} role was added.");
-        }
+        return Ok($"{roleName} role was added.");
+    }
 
-        [Route("user-role")]
-        [HttpGet]
-        public async Task<IActionResult> GetUserRoles()
-        {
-            var userRoles = await _userRolesRepository.GetUserRoles();
+    [Route("user-role")]
+    [HttpGet]
+    public async Task<IActionResult> GetUserRoles()
+    {
+        var userRoles = await _userRolesRepository.GetUserRoles();
 
-            return Ok(userRoles);
-        }
+        return Ok(userRoles);
+    }
 
-        [Route("user-role")]
-        [HttpPost]
-        public async Task<IActionResult> AddUserToRole(UserRoleDto userRole)
-        {
-            var roleName = userRole.RoleName.ToLower();
+    [Route("user-role")]
+    [HttpPost]
+    public async Task<IActionResult> AddUserToRole(UserRoleDto userRole)
+    {
+        var roleName = userRole.RoleName.ToLower();
             
-            var userExists = await _userRepository.DoesUserExist(userRole.UserId);
+        var userExists = await _userRepository.DoesUserExist(userRole.UserId);
 
-            if (!userExists)
-            {
-                return NotFound("User doesn't exist");
-            }
-
-            var roleExists = await _userRolesRepository.DoesRoleExist(userRole.RoleName);
-
-            if (!roleExists)
-            {
-                return NotFound("Role doesn't exist.");
-            }
-
-            var isOperationSuccessfull = await _userRolesRepository.SetUserRole(userRole.UserId, roleName);
-
-            if (!isOperationSuccessfull)
-            {
-                return BadRequest("Something went wrong.");
-            }
-
-            return Ok("Successfully added user to role");
+        if (!userExists)
+        {
+            return NotFound("User doesn't exist");
         }
+
+        var roleExists = await _userRolesRepository.DoesRoleExist(userRole.RoleName);
+
+        if (!roleExists)
+        {
+            return NotFound("Role doesn't exist.");
+        }
+
+        var isOperationSuccessfull = await _userRolesRepository.SetUserRole(userRole.UserId, roleName);
+
+        if (!isOperationSuccessfull)
+        {
+            return BadRequest("Something went wrong.");
+        }
+
+        return Ok("Successfully added user to role");
     }
 }
