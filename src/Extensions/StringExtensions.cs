b9f939a7ed6dcc24ec8cf9a819f2eb5decd85cf4
Eveloth <vadim.stepanov.web@gmail.com>
2022-11-10 23:07:26 +0300

    feat: add extensions library for ext. methods in common use

---
+++ src/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Extensions;
+
+public static class StringExtensions
+{
+    public static string Subtract(this string input, string substring)
+    {
+        return input.Contains(substring) ? input.Replace(substring, string.Empty) : input;
+    }
+}
