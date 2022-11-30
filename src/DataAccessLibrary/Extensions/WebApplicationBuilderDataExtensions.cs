440c27d69ac23eb3eda6d284512e0f55f01760e6
eveloth <vadim.stepanov.web@gmail.com>
2022-11-30 22:12:12 +0300

    chore: add extension methods for app configuration

---
+++ src/DataAccessLibrary/Extensions/WebApplicationBuilderDataExtensions.cs
@@ -0,0 +1,83 @@
+﻿using System.Data;
+using System.Data.Common;
+using Dapper;
+using DataAccessLibrary.Repository;
+using DataAccessLibrary.Repository.Interfaces;
+using DataAccessLibrary.SqlDataAccess;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace DataAccessLibrary.Extensions;
+
+public static class WebApplicationBuilderDataExtensions
+{
+    public static WebApplicationBuilder AddNpgsql(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IDbConnection>(
+            _ => new NpgsqlConnection(builder.Configuration.GetConnectionString("Default"))
+        );
+        builder.Services.AddScoped<DbTransaction>(s =>
+        {
+            var connection = s.GetRequiredService<IDbConnection>();
+            connection.Open();
+            return (DbTransaction)connection.BeginTransaction();
+        });
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddDapperSnakeCaseConvention(
+        this WebApplicationBuilder builder
+    )
+    {
+        DefaultTypeMap.MatchNamesWithUnderscores = true;
+        return builder;
+    }
+
+    public static WebApplicationBuilder UseAffixForModelMapping(
+        this WebApplicationBuilder builder,
+        string affix
+    )
+    {
+        SqlDataAccessOptions.HasAffix = true;
+        SqlDataAccessOptions.Affix = affix;
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder UseCustomSplitOn(
+        this WebApplicationBuilder builder,
+        string splitOn
+    )
+    {
+        SqlDataAccessOptions.SplitOn = splitOn;
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddRepositories(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<ISqlDataAccess, SqlDataAccess.SqlDataAccess>();
+
+        builder.Services.AddScoped<IGeneralPurposeRepository, GeneralPurposeRepository>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+        builder.Services.AddScoped<IRolePermissionRepository, RolePermissionRepository>();
+        builder.Services.AddScoped<IPermissionRepository, PermissionRepository>();
+        builder.Services.AddScoped<IExtCredentialsRepository, ExtCredentialsRepository>();
+        builder.Services.AddScoped<IIssueTypeRepository, IssueTypeRepository>();
+        builder.Services.AddScoped<IIssueRepository, IssueRepository>();
+        builder.Services.AddScoped<IRuleRepository, RuleRepository>();
+        builder.Services.AddScoped<
+            IEvApiQueryParametersRepository,
+            EvApiQueryParametersRepository
+        >();
+        builder.Services.AddScoped<IMailAnalysisRulesRepository, MailAnalysisRulesRepository>();
+        builder.Services.AddScoped<IIssueFieldRepository, IssueFieldRepository>();
+        builder.Services.AddScoped<IUnitofWork, UnitofWork>();
+
+        return builder;
+    }
+}
--- src/EvAutoreg/Program.cs
+++ src/EvAutoreg/Program.cs
@@ -1,23 +1,11 @@
-using System.Data;
-using System.Data.Common;
-using System.Reflection;
-using System.Text;
-using Dapper;
 using DataAccessLibrary.Extensions;
-using DataAccessLibrary.Repository;
-using DataAccessLibrary.Repository.Interfaces;
-using DataAccessLibrary.SqlDataAccess;
-using EvAutoreg.Exceptions;
+using EvAutoreg.Extensions;
 using EvAutoreg.Mapping;
 using EvAutoreg.Middleware;
 using EvAutoreg.Services;
 using EvAutoreg.Services.Interfaces;
 using MapsterMapper;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using Npgsql;
 using Serilog;
 
 namespace EvAutoreg;
@@ -48,133 +36,16 @@ internal static class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
 
-        #region Swagger
-
-        builder.Services.AddSwaggerGen(options =>
-        {
-            options.SwaggerDoc("v1", new OpenApiInfo { Title = "EvAutoreg", Version = "v0.4.0" });
-
-            options.AddSecurityDefinition(
-                "Bearer",
-                new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please enter an issued token",
-                    Name = "Authrization",
-                    Type = SecuritySchemeType.Http,
-                    BearerFormat = "JWT",
-                    Scheme = "Bearer"
-                }
-            );
-
-            options.AddSecurityRequirement(
-                new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        Array.Empty<string>()
-                    }
-                }
-            );
-
-            var xmlCommentsFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlCommentsFileName));
-        });
-
-        #endregion
-
+        builder.AddSwagger();
         builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
-        #region JWT Authentication
+        builder.AddJwtAuthentication().AddPolicyBasedAuthorization();
 
-        builder.Services
-            .AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(t =>
-            {
-                t.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidIssuer =
-                        builder.Configuration["Jwt:Issuer"]
-                        ?? throw new NullConfigurationEntryException(
-                            "Could not read configuration for JWT"
-                        ),
-                    IssuerSigningKey = new SymmetricSecurityKey(
-                        Encoding.UTF8.GetBytes(
-                            builder.Configuration["Jwt:Key"]
-                                ?? throw new NullConfigurationEntryException(
-                                    "Could not read configuration for JWT"
-                                )
-                        )
-                    ),
-                    ValidateIssuer = true,
-                    ValidateAudience = false,
-                    ValidateLifetime = false,
-                    ValidateIssuerSigningKey = true
-                };
-            });
-
-        #endregion
-
-        #region Policy-based Authorization
-
-        builder.Services.AddAuthorization(options =>
-        {
-            var userPermissions = Permissions.GetPermissions();
-
-            foreach (var permission in userPermissions)
-            {
-                options.AddPolicy(
-                    $"{permission.PermissionName}",
-                    policy => policy.RequireClaim("Permission", $"{permission.PermissionName}")
-                );
-            }
-        });
-
-        #endregion
-
-        #region SqlDataAccess
-
-        builder.Services.AddScoped<IDbConnection>(
-            _ => new NpgsqlConnection(builder.Configuration.GetConnectionString("Default"))
-        );
-        builder.Services.AddScoped<DbTransaction>(s =>
-        {
-            var connection = s.GetRequiredService<IDbConnection>();
-            connection.Open();
-            return (DbTransaction)connection.BeginTransaction();
-        });
-
-        builder.Services.ConfigureSqlDataAccess().UseAffixForModelMapping("Model");
-        builder.Services.AddScoped<ISqlDataAccess, SqlDataAccess>();
-
-        builder.Services.AddScoped<IGeneralPurposeRepository, GeneralPurposeRepository>();
-        builder.Services.AddScoped<IUserRepository, UserRepository>();
-        builder.Services.AddScoped<IRoleRepository, RoleRepository>();
-        builder.Services.AddScoped<IRolePermissionRepository, RolePermissionRepository>();
-        builder.Services.AddScoped<IPermissionRepository, PermissionRepository>();
-        builder.Services.AddScoped<IExtCredentialsRepository, ExtCredentialsRepository>();
-        builder.Services.AddScoped<IIssueTypeRepository, IssueTypeRepository>();
-        builder.Services.AddScoped<IIssueRepository, IssueRepository>();
-        builder.Services.AddScoped<IRuleRepository, RuleRepository>();
-        builder.Services.AddScoped<
-            IEvApiQueryParametersRepository,
-            EvApiQueryParametersRepository
-        >();
-        builder.Services.AddScoped<IMailAnalysisRulesRepository, MailAnalysisRulesRepository>();
-        builder.Services.AddScoped<IIssueFieldRepository, IssueFieldRepository>();
-        builder.Services.AddScoped<IUnitofWork, UnitofWork>();
+        builder
+            .AddNpgsql()
+            .UseAffixForModelMapping("Model")
+            .AddDapperSnakeCaseConvention()
+            .AddRepositories();
 
         builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
         builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
@@ -182,14 +53,11 @@ internal static class Program
 
         builder.Services.AddTransient<DatabaseSeeder>();
 
-        #endregion
-
         builder.Services.AddSingleton<IMapper, Mapper>();
         builder.Services.AddGrpcClient<Registrar.RegistrarClient>(options =>
         {
             options.Address = new Uri("https://localhost:7037");
         });
-
 
         var app = builder.Build();
 
@@ -210,8 +78,6 @@ internal static class Program
         app.UseMiddleware<ErrorHandlingMiddleware>();
 
         app.MapControllers();
-
-        DefaultTypeMap.MatchNamesWithUnderscores = true;
 
         using (var scope = app.Services.CreateScope())
         {
