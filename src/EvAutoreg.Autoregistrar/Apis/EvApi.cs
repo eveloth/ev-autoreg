8f48c6311e0b470a7613d8e7af012546de3cb18b
eveloth <vadim.stepanov.web@gmail.com>
2023-02-03 10:12:08 +0300

    chore: use StringBuilder in EV API wrapper

--- src/EvAutoreg.Autoregistrar/Apis/EvApi.cs
+++ src/EvAutoreg.Autoregistrar/Apis/EvApi.cs
@@ -1,7 +1,8 @@
-using System.Net;
+using System.Text;
 using EvAutoreg.Autoregistrar.Domain;
 using EvAutoreg.Autoregistrar.Settings;
 using EvAutoreg.Extensions;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EvAutoreg.Autoregistrar.Apis;
 
@@ -14,6 +15,16 @@ public class EvApi : IEvApi
     private readonly string _evEmail = GlobalSettings.ExtraViewCredentials!.Email;
     private readonly string _evPassword = GlobalSettings.ExtraViewCredentials.Password;
 
+    private const string Https = "https://";
+    private const string EvBackend = "/evj/ExtraView/";
+    private const string EvAction = "ev.api.action?";
+    private const string EvUserId = "user_id=";
+    private const string EvPassword = "&password=";
+    private const string MethodCall = "&statevar=";
+    private const string GetMethod = "get";
+    private const string UpdateMethod = "update";
+    private const string IssueId = "&id=";
+
     public static string ClientName => nameof(EvApi) + "Client";
 
     public EvApi(ILogger<EvApi> logger, IHttpClientFactory httpClientFactory)
@@ -25,10 +36,20 @@ public class EvApi : IEvApi
     public async Task<XmlIssue> GetIssue(string issueNo)
     {
         var client = _httpClientFactory.CreateClient(ClientName);
-        var query =
-            $"https://{_evUri}/"
-            + $"evj/ExtraView/ev_api.action?user_id={_evEmail}"
-            + $"&password={_evPassword}&statevar=get&id={issueNo}";
+        var queryBuilder = new StringBuilder(Https)
+            .Append(_evUri)
+            .Append(EvBackend)
+            .Append(EvAction)
+            .Append(EvUserId)
+            .Append(_evEmail)
+            .Append(EvPassword)
+            .Append(_evPassword)
+            .Append(MethodCall)
+            .Append(GetMethod)
+            .Append(IssueId)
+            .Append(issueNo);
+
+        var query = queryBuilder.ToString();
 
         var response = await client.GetAsync(query);
         var responseBody = await response.Content.ReadAsStringAsync();
@@ -38,7 +59,7 @@ public class EvApi : IEvApi
             throw new EvApiException(responseBody, response.StatusCode);
         }
 
-        if (!responseBody.StartsWith("<?xml)"))
+        if (!IsValidXml(responseBody))
         {
             throw new EvApiException(responseBody);
         }
@@ -49,15 +70,28 @@ public class EvApi : IEvApi
     public async Task UpdateIssue(string issueNo, params string[] queryParameters)
     {
         var client = _httpClientFactory.CreateClient(ClientName);
-        var query =
-            $"https://{_evUri}/"
-            + $"evj/ExtraView/ev_api.action?user_id={_evEmail}"
-            + $"&password={_evPassword}&statevar=update&id={issueNo}";
+        var queryBuilder = new StringBuilder(Https)
+            .Append(_evUri)
+            .Append(EvBackend)
+            .Append(EvAction)
+            .Append(EvUserId)
+            .Append(_evEmail)
+            .Append(EvPassword)
+            .Append(_evPassword)
+            .Append(MethodCall)
+            .Append(UpdateMethod)
+            .Append(IssueId)
+            .Append(issueNo);
 
         foreach (var param in queryParameters)
         {
-            query += $"&{param}";
+            if (!param.IsNullOrEmpty())
+            {
+                queryBuilder.Append('&').Append(param);
+            }
         }
+
+        var query = queryBuilder.ToString();
 
         var response = await client.GetAsync(query);
         var responseBody = await response.Content.ReadAsStringAsync();
@@ -67,9 +101,14 @@ public class EvApi : IEvApi
             throw new EvApiException(responseBody, response.StatusCode);
         }
 
-        if (!responseBody.Contains("updated", StringComparison.InvariantCultureIgnoreCase))
+        if (!responseBody.Contains("updated", StringComparison.OrdinalIgnoreCase))
         {
             throw new EvApiException(responseBody);
         }
+    }
+
+    private static bool IsValidXml(string xmlString)
+    {
+        return xmlString.StartsWith("<?xml");
     }
 }
