8254374251e2e87a563ed94dda27b9bcaee6a433
eveloth <vadim.stepanov.web@gmail.com>
2023-02-08 00:56:11 +0300

    chore: remove console app projects
    moved to an archived repo as in museum

--- src/EvAutoreg.Console/Interfaces/IMailEventListener.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Exchange.WebServices.Data;
-using Task = System.Threading.Tasks.Task;
-
-namespace EvAutoreg.Console.Interfaces;
-
-public interface IMailEventListener
-{
-    Task ProcessEvent(EmailMessage email);
-}
